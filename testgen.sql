d78b456257f16c0a69e7b9a49413059b992418a8
Vicvip23 <wicekwiacek@gmail.com>
2024-03-18 09:00:31 +0100

    changed db structure

--- testgen.sql
+++ testgen.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 17 Mar 2024, 20:19
--- Wersja serwera: 10.4.27-MariaDB
--- Wersja PHP: 8.2.0
+-- Generation Time: Mar 18, 2024 at 08:51 AM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,30 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `testgen`
+-- Database: `testgen`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `answers`
+--
+
+CREATE TABLE `answers` (
+  `id` int(11) NOT NULL,
+  `questionID` int(11) NOT NULL,
+  `answerCorrect` varchar(32) DEFAULT NULL,
+  `answerWrong1` varchar(32) DEFAULT NULL,
+  `answerWrong2` varchar(32) DEFAULT NULL,
+  `answerWrong3` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `answers`
+--
+
+INSERT INTO `answers` (`id`, `questionID`, `answerCorrect`, `answerWrong1`, `answerWrong2`, `answerWrong3`) VALUES
+(1, 1, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -30,20 +52,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `questions` (
   `id` int(11) NOT NULL,
   `question` varchar(64) NOT NULL,
-  `answer` varchar(32) DEFAULT NULL,
   `createdAt` date DEFAULT current_timestamp(),
   `modifiedAt` date DEFAULT current_timestamp(),
-  `addedByID` int(11) DEFAULT NULL
+  `addedByID` int(11) DEFAULT NULL,
+  `answerID` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `questions`
+-- Dumping data for table `questions`
 --
 
-INSERT INTO `questions` (`id`, `question`, `answer`, `createdAt`, `modifiedAt`, `addedByID`) VALUES
-(1, 'how much is 2 + 2', '4', '2024-03-06', '2024-03-06', NULL),
-(2, 'how much is 2 + 3', '5', '2024-03-06', '2024-03-06', NULL),
-(3, 'how much is 2 + 4', '6', '2024-03-06', '2024-03-06', NULL);
+INSERT INTO `questions` (`id`, `question`, `createdAt`, `modifiedAt`, `addedByID`, `answerID`) VALUES
+(1, 'how much is 2 + 2', '2024-03-06', '2024-03-06', NULL, 1),
+(2, 'how much is 2 + 3', '2024-03-06', '2024-03-06', NULL, 1),
+(3, 'how much is 2 + 4', '2024-03-06', '2024-03-06', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -63,11 +85,19 @@ CREATE TABLE `users` (
 --
 
 --
+-- Indeksy dla tabeli `answers`
+--
+ALTER TABLE `answers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `questionID` (`questionID`);
+
+--
 -- Indeksy dla tabeli `questions`
 --
 ALTER TABLE `questions`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `addedByID` (`addedByID`);
+  ADD KEY `addedByID` (`addedByID`),
+  ADD KEY `answerID` (`answerID`);
 
 --
 -- Indeksy dla tabeli `users`
@@ -76,30 +106,43 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT dla zrzuconych tabel
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `questions`
+-- AUTO_INCREMENT for table `answers`
+--
+ALTER TABLE `answers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT dla tabeli `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Ograniczenia dla zrzutów tabel
+-- Constraints for dumped tables
 --
 
 --
--- Ograniczenia dla tabeli `questions`
+-- Constraints for table `answers`
+--
+ALTER TABLE `answers`
+  ADD CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`questionID`) REFERENCES `questions` (`id`);
+
+--
+-- Constraints for table `questions`
 --
 ALTER TABLE `questions`
-  ADD CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`addedByID`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`addedByID`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `questions_ibfk_2` FOREIGN KEY (`answerID`) REFERENCES `answers` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
